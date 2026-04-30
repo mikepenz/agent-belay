gabrielpopa/agent-approver

Opencode integration as plugin

composeApp/src/jvmMain/kotlin/com/mikepenz/agentbuddy/di/AppProviders.kt
@@ -6,7 +6,9 @@ import com.mikepenz.agentbuddy.capability.modules.SocraticThinkingCapability
 import com.mikepenz.agentbuddy.hook.CopilotBridge
 import com.mikepenz.agentbuddy.hook.DefaultCopilotBridge
 import com.mikepenz.agentbuddy.hook.DefaultHookRegistry
+import com.mikepenz.agentbuddy.hook.DefaultOpenCodeBridge
 import com.mikepenz.agentbuddy.hook.HookRegistry
+import com.mikepenz.agentbuddy.hook.OpenCodeBridge
 import com.mikepenz.agentbuddy.protection.ProtectionEngine
 import com.mikepenz.agentbuddy.protection.modules.AbsolutePathsModule
 import com.mikepenz.agentbuddy.protection.modules.DestructiveCommandsModule
@@ -107,6 +109,10 @@ interface AppProviders {
     @Provides
     @SingleIn(AppScope::class)
     fun provideCopilotBridge(): CopilotBridge = DefaultCopilotBridge
+
+    @Provides
+    @SingleIn(AppScope::class)
+    fun provideOpenCodeBridge(): OpenCodeBridge = DefaultOpenCodeBridge
 
     @Provides
     @SingleIn(AppScope::class)

composeApp/src/jvmMain/kotlin/com/mikepenz/agentbuddy/ui/AppViewModel.kt
@@ -7,6 +7,7 @@ import com.mikepenz.agentbuddy.di.AppEnvironment
 import com.mikepenz.agentbuddy.di.AppScope
 import com.mikepenz.agentbuddy.hook.CopilotBridge
 import com.mikepenz.agentbuddy.hook.HookRegistry
+import com.mikepenz.agentbuddy.hook.OpenCodeBridge
 import com.mikepenz.agentbuddy.hook.RegistrationEvents
 import com.mikepenz.agentbuddy.state.AppNotice
 import com.mikepenz.agentbuddy.state.AppStateManager
@@ -40,6 +41,7 @@ class AppViewModel(
     env: AppEnvironment,
     private val hookRegistry: HookRegistry,
     private val copilotBridge: CopilotBridge,
+    private val openCodeBridge: OpenCodeBridge,
     private val registrationEvents: RegistrationEvents,
     private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO,
 ) : ViewModel() {
@@ -50,12 +52,14 @@ class AppViewModel(
     /** Re-polled whenever the configured `serverPort` changes. */
     private val claudeRegistered = MutableStateFlow(false)
     private val copilotRegistered = MutableStateFlow(false)
+    private val openCodeRegistered = MutableStateFlow(false)
 
     val tabState: StateFlow<TabState> = combine(
         stateManager.state,
         claudeRegistered,
         copilotRegistered,
-    ) { state, claude, copilot ->
+        openCodeRegistered,
+    ) { state, claude, copilot, openCode ->
         TabState(
             pendingCount = state.pendingApprovals.size,
             protectionLogCount = state.preToolUseLog.size,
@@ -66,6 +70,7 @@ class AppViewModel(
             agentRegistrations = listOf(
                 AgentRegistration("Claude Code", claude),
                 AgentRegistration("GitHub Copilot", copilot),
+                AgentRegistration("OpenCode", openCode),
             ),
         )
     }.stateIn(
@@ -91,11 +96,16 @@ class AppViewModel(
                 .map { it.settings.serverPort }
                 .distinctUntilChanged()
                 .collect { port ->
-                    val (c, cp) = withContext(ioDispatcher) {
-                        hookRegistry.isRegistered(port) to copilotBridge.isRegistered(port)
+                    val (c, cp, oc) = withContext(ioDispatcher) {
+                        Triple(
+                            hookRegistry.isRegistered(port),
+                            copilotBridge.isRegistered(port),
+                            openCodeBridge.isRegistered(port),
+                        )
                     }
                     claudeRegistered.value = c
                     copilotRegistered.value = cp
+                    openCodeRegistered.value = oc
                 }
         }
         // Re-poll when SettingsViewModel registers or unregisters a hook so
@@ -103,11 +113,16 @@ class AppViewModel(
         viewModelScope.launch {
             registrationEvents.changes.collect {
                 val port = stateManager.state.value.settings.serverPort
-                val (c, cp) = withContext(ioDispatcher) {
-                    hookRegistry.isRegistered(port) to copilotBridge.isRegistered(port)
+                val (c, cp, oc) = withContext(ioDispatcher) {
+                    Triple(
+                        hookRegistry.isRegistered(port),
+                        copilotBridge.isRegistered(port),
+                        openCodeBridge.isRegistered(port),
+                    )
                 }
                 claudeRegistered.value = c
                 copilotRegistered.value = cp
+                openCodeRegistered.value = oc
             }
         }
     }

composeApp/src/jvmMain/kotlin/com/mikepenz/agentbuddy/ui/theme/Theme.kt
@@ -91,15 +91,18 @@ val ToolWebColor = ToolWeb               // oklch(0.78 0.13 48)
 // Source badge colors
 val SourceClaudeColor = Color(0xFFD97757)  // oklch(0.78 0.14 30) — Claude orange
 val SourceCopilotColor = VioletPurple      // oklch(0.74 0.14 290) — design-system violet
+val SourceOpenCodeColor = Color(0xFF10B981) // emerald green — OpenCode
 
 fun sourceColor(source: com.mikepenz.agentbuddy.model.Source): Color = when (source) {
     com.mikepenz.agentbuddy.model.Source.CLAUDE_CODE -> SourceClaudeColor
     com.mikepenz.agentbuddy.model.Source.COPILOT -> SourceCopilotColor
+    com.mikepenz.agentbuddy.model.Source.OPENCODE -> SourceOpenCodeColor
 }
 
 fun sourceLabel(source: com.mikepenz.agentbuddy.model.Source): String = when (source) {
     com.mikepenz.agentbuddy.model.Source.CLAUDE_CODE -> "Claude Code"
     com.mikepenz.agentbuddy.model.Source.COPILOT -> "Copilot"
+    com.mikepenz.agentbuddy.model.Source.OPENCODE -> "OpenCode"
 }
 
 fun riskColor(risk: Int): Color = when (risk) {

composeApp/src/jvmTest/kotlin/com/mikepenz/agentbuddy/ui/AppViewModelTest.kt
@@ -3,6 +3,7 @@ package com.mikepenz.agentbuddy.ui
 import com.mikepenz.agentbuddy.di.AppEnvironment
 import com.mikepenz.agentbuddy.hook.CopilotBridge
 import com.mikepenz.agentbuddy.hook.HookRegistry
+import com.mikepenz.agentbuddy.hook.OpenCodeBridge
 import com.mikepenz.agentbuddy.hook.RegistrationEvents
 import com.mikepenz.agentbuddy.model.ApprovalRequest
 import com.mikepenz.agentbuddy.model.HookInput
@@ -67,6 +68,15 @@ class AppViewModelTest {
         override fun unregisterCapabilityHook(port: Int) {}
     }
 
+    private object FakeOpenCodeBridge : OpenCodeBridge {
+        override fun isRegistered(port: Int): Boolean = false
+        override fun register(port: Int) {}
+        override fun unregister(port: Int) {}
+        override fun isCapabilityHookRegistered(port: Int): Boolean = false
+        override fun registerCapabilityHook(port: Int) {}
+        override fun unregisterCapabilityHook(port: Int) {}
+    }
+
     private fun newRequest(id: String = "r-1") = ApprovalRequest(
         id = id,
         source = Source.CLAUDE_CODE,
@@ -79,7 +89,7 @@ class AppViewModelTest {
     @Test
     fun `tabState reflects pending count and away mode`() = runTest {
         val state = AppStateManager()
-        val vm = AppViewModel(state, env(devMode = false), FakeHookRegistry, FakeCopilotBridge, RegistrationEvents())
+        val vm = AppViewModel(state, env(devMode = false), FakeHookRegistry, FakeCopilotBridge, FakeOpenCodeBridge, RegistrationEvents())
         runCurrent()
 
         assertEquals(0, vm.tabState.value.pendingCount)
@@ -100,7 +110,7 @@ class AppViewModelTest {
 
     @Test
     fun `selectTab updates the selected index`() = runTest {
-        val vm = AppViewModel(AppStateManager(), env(), FakeHookRegistry, FakeCopilotBridge, RegistrationEvents())
+        val vm = AppViewModel(AppStateManager(), env(), FakeHookRegistry, FakeCopilotBridge, FakeOpenCodeBridge, RegistrationEvents())
         runCurrent()
 
         assertEquals(0, vm.selectedTab.value)
@@ -110,7 +120,7 @@ class AppViewModelTest {
 
     @Test
     fun `devMode flag comes from environment`() = runTest {
-        val vm = AppViewModel(AppStateManager(), env(devMode = true), FakeHookRegistry, FakeCopilotBridge, RegistrationEvents())
+        val vm = AppViewModel(AppStateManager(), env(devMode = true), FakeHookRegistry, FakeCopilotBridge, FakeOpenCodeBridge, RegistrationEvents())
         runCurrent()
         assertTrue(vm.tabState.value.devMode)
     }

composeApp/src/jvmTest/kotlin/com/mikepenz/agentbuddy/ui/settings/SettingsViewModelTest.kt
@@ -5,6 +5,7 @@ import com.mikepenz.agentbuddy.capability.modules.ResponseCompressionCapability
 import com.mikepenz.agentbuddy.capability.modules.SocraticThinkingCapability
 import com.mikepenz.agentbuddy.hook.CopilotBridge
 import com.mikepenz.agentbuddy.hook.HookRegistry
+import com.mikepenz.agentbuddy.hook.OpenCodeBridge
 import com.mikepenz.agentbuddy.hook.RegistrationEvents
 import com.mikepenz.agentbuddy.model.CapabilityModuleSettings
 import com.mikepenz.agentbuddy.model.CapabilitySettings
@@ -108,6 +109,18 @@ class SettingsViewModelTest {
         override fun unregisterSessionStartHook(port: Int) { sessionStartHookPorts.remove(port) }
     }
 
+    private class FakeOpenCodeBridge : OpenCodeBridge {
+        val registeredPorts: MutableSet<Int> = mutableSetOf()
+        override fun isRegistered(port: Int): Boolean = port in registeredPorts
+        override fun register(port: Int) { registeredPorts.add(port) }
+        override fun unregister(port: Int) { registeredPorts.remove(port) }
+
+        val capabilityHookPorts: MutableSet<Int> = mutableSetOf()
+        override fun isCapabilityHookRegistered(port: Int): Boolean = port in capabilityHookPorts
+        override fun registerCapabilityHook(port: Int) { capabilityHookPorts.add(port) }
+        override fun unregisterCapabilityHook(port: Int) { capabilityHookPorts.remove(port) }
+    }
+
     private fun newVm(
         bridge: FakeCopilotBridge = FakeCopilotBridge(),
         registry: FakeHookRegistry = FakeHookRegistry(),
@@ -131,6 +144,7 @@ class SettingsViewModelTest {
         val vm = SettingsViewModel(
             stateManager = state,
             copilotBridge = bridge,
+            openCodeBridge = FakeOpenCodeBridge(),
             copilotStateHolder = copilotState,
             ollamaStateHolder = ollamaState,
             riskAnalyzerLifecycle = lifecycle,

mikepenz/agent-belay

composeApp/src/jvmMain/kotlin/com/mikepenz/agentbelay/di/AppProviders.kt
@@ -6,7 +6,9 @@ import com.mikepenz.agentbelay.capability.modules.SocraticThinkingCapability
 import com.mikepenz.agentbelay.hook.CopilotBridge
 import com.mikepenz.agentbelay.hook.DefaultCopilotBridge
 import com.mikepenz.agentbelay.hook.DefaultHookRegistry
+import com.mikepenz.agentbelay.hook.DefaultOpenCodeBridge
 import com.mikepenz.agentbelay.hook.HookRegistry
+import com.mikepenz.agentbelay.hook.OpenCodeBridge
 import com.mikepenz.agentbelay.protection.ProtectionEngine
 import com.mikepenz.agentbelay.protection.modules.AbsolutePathsModule
 import com.mikepenz.agentbelay.protection.modules.DestructiveCommandsModule
@@ -115,6 +117,10 @@ interface AppProviders {
     @Provides
     @SingleIn(AppScope::class)
     fun provideCopilotBridge(): CopilotBridge = DefaultCopilotBridge
+
+    @Provides
+    @SingleIn(AppScope::class)
+    fun provideOpenCodeBridge(): OpenCodeBridge = DefaultOpenCodeBridge
 
     @Provides
     @SingleIn(AppScope::class)

composeApp/src/jvmMain/kotlin/com/mikepenz/agentbelay/ui/AppViewModel.kt
@@ -7,6 +7,7 @@ import com.mikepenz.agentbelay.di.AppEnvironment
 import com.mikepenz.agentbelay.di.AppScope
 import com.mikepenz.agentbelay.hook.CopilotBridge
 import com.mikepenz.agentbelay.hook.HookRegistry
+import com.mikepenz.agentbelay.hook.OpenCodeBridge
 import com.mikepenz.agentbelay.hook.RegistrationEvents
 import com.mikepenz.agentbelay.state.AppNotice
 import com.mikepenz.agentbelay.state.AppStateManager
@@ -42,6 +43,7 @@ class AppViewModel(
     env: AppEnvironment,
     private val hookRegistry: HookRegistry,
     private val copilotBridge: CopilotBridge,
+    private val openCodeBridge: OpenCodeBridge,
     private val registrationEvents: RegistrationEvents,
     private val updateManager: UpdateManager,
     private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO,
@@ -65,12 +67,14 @@ class AppViewModel(
     /** Re-polled whenever the configured `serverPort` changes. */
     private val claudeRegistered = MutableStateFlow(false)
     private val copilotRegistered = MutableStateFlow(false)
+    private val openCodeRegistered = MutableStateFlow(false)
 
     val tabState: StateFlow<TabState> = combine(
         stateManager.state,
         claudeRegistered,
         copilotRegistered,
-    ) { state, claude, copilot ->
+        openCodeRegistered,
+    ) { state, claude, copilot, openCode ->
         TabState(
             pendingCount = state.pendingApprovals.size,
             protectionLogCount = state.preToolUseLog.size,
@@ -81,6 +85,7 @@ class AppViewModel(
             agentRegistrations = listOf(
                 AgentRegistration("Claude Code", claude),
                 AgentRegistration("GitHub Copilot", copilot),
+                AgentRegistration("OpenCode", openCode),
             ),
         )
     }.stateIn(
@@ -106,11 +111,16 @@ class AppViewModel(
                 .map { it.settings.serverPort }
                 .distinctUntilChanged()
                 .collect { port ->
-                    val (c, cp) = withContext(ioDispatcher) {
-                        hookRegistry.isRegistered(port) to copilotBridge.isRegistered(port)
+                    val (c, cp, oc) = withContext(ioDispatcher) {
+                        Triple(
+                            hookRegistry.isRegistered(port),
+                            copilotBridge.isRegistered(port),
+                            openCodeBridge.isRegistered(port),
+                        )
                     }
                     claudeRegistered.value = c
                     copilotRegistered.value = cp
+                    openCodeRegistered.value = oc
                 }
         }
         // Re-poll when SettingsViewModel registers or unregisters a hook so
@@ -118,11 +128,16 @@ class AppViewModel(
         viewModelScope.launch {
             registrationEvents.changes.collect {
                 val port = stateManager.state.value.settings.serverPort
-                val (c, cp) = withContext(ioDispatcher) {
-                    hookRegistry.isRegistered(port) to copilotBridge.isRegistered(port)
+                val (c, cp, oc) = withContext(ioDispatcher) {
+                    Triple(
+                        hookRegistry.isRegistered(port),
+                        copilotBridge.isRegistered(port),
+                        openCodeBridge.isRegistered(port),
+                    )
                 }
                 claudeRegistered.value = c
                 copilotRegistered.value = cp
+                openCodeRegistered.value = oc
             }
         }
     }

composeApp/src/jvmMain/kotlin/com/mikepenz/agentbelay/ui/theme/Theme.kt
@@ -91,15 +91,18 @@ val ToolWebColor = ToolWeb               // oklch(0.78 0.13 48)
 // Source badge colors
 val SourceClaudeColor = Color(0xFFD97757)  // oklch(0.78 0.14 30) — Claude orange
 val SourceCopilotColor = VioletPurple      // oklch(0.74 0.14 290) — design-system violet
+val SourceOpenCodeColor = Color(0xFF10B981) // emerald green — OpenCode
 
 fun sourceColor(source: com.mikepenz.agentbelay.model.Source): Color = when (source) {
     com.mikepenz.agentbelay.model.Source.CLAUDE_CODE -> SourceClaudeColor
     com.mikepenz.agentbelay.model.Source.COPILOT -> SourceCopilotColor
+    com.mikepenz.agentbelay.model.Source.OPENCODE -> SourceOpenCodeColor
 }
 
 fun sourceLabel(source: com.mikepenz.agentbelay.model.Source): String = when (source) {
     com.mikepenz.agentbelay.model.Source.CLAUDE_CODE -> "Claude Code"
     com.mikepenz.agentbelay.model.Source.COPILOT -> "Copilot"
+    com.mikepenz.agentbelay.model.Source.OPENCODE -> "OpenCode"
 }
 
 fun riskColor(risk: Int): Color = when (risk) {

composeApp/src/jvmTest/kotlin/com/mikepenz/agentbelay/ui/AppViewModelTest.kt
@@ -3,6 +3,7 @@ package com.mikepenz.agentbelay.ui
 import com.mikepenz.agentbelay.di.AppEnvironment
 import com.mikepenz.agentbelay.hook.CopilotBridge
 import com.mikepenz.agentbelay.hook.HookRegistry
+import com.mikepenz.agentbelay.hook.OpenCodeBridge
 import com.mikepenz.agentbelay.hook.RegistrationEvents
 import com.mikepenz.agentbelay.model.ApprovalRequest
 import com.mikepenz.agentbelay.model.HookInput
@@ -68,6 +69,15 @@ class AppViewModelTest {
         override fun unregisterCapabilityHook(port: Int) {}
     }
 
+    private object FakeOpenCodeBridge : OpenCodeBridge {
+        override fun isRegistered(port: Int): Boolean = false
+        override fun register(port: Int) {}
+        override fun unregister(port: Int) {}
+        override fun isCapabilityHookRegistered(port: Int): Boolean = false
+        override fun registerCapabilityHook(port: Int) {}
+        override fun unregisterCapabilityHook(port: Int) {}
+    }
+
     private fun fakeUpdateManager() = UpdateManager(scope = CoroutineScope(SupervisorJob()))
 
     private fun newRequest(id: String = "r-1") = ApprovalRequest(
@@ -82,7 +92,7 @@ class AppViewModelTest {
     @Test
     fun `tabState reflects pending count and away mode`() = runTest {
         val state = AppStateManager()
-        val vm = AppViewModel(state, env(devMode = false), FakeHookRegistry, FakeCopilotBridge, RegistrationEvents(), fakeUpdateManager())
+        val vm = AppViewModel(state, env(devMode = false), FakeHookRegistry, FakeCopilotBridge, FakeOpenCodeBridge, RegistrationEvents(), fakeUpdateManager())
         runCurrent()
 
         assertEquals(0, vm.tabState.value.pendingCount)
@@ -103,7 +113,7 @@ class AppViewModelTest {
 
     @Test
     fun `selectTab updates the selected index`() = runTest {
-        val vm = AppViewModel(AppStateManager(), env(), FakeHookRegistry, FakeCopilotBridge, RegistrationEvents(), fakeUpdateManager())
+        val vm = AppViewModel(AppStateManager(), env(), FakeHookRegistry, FakeCopilotBridge, FakeOpenCodeBridge, RegistrationEvents(), fakeUpdateManager())
         runCurrent()
 
         assertEquals(0, vm.selectedTab.value)
@@ -113,7 +123,7 @@ class AppViewModelTest {
 
     @Test
     fun `devMode flag comes from environment`() = runTest {
-        val vm = AppViewModel(AppStateManager(), env(devMode = true), FakeHookRegistry, FakeCopilotBridge, RegistrationEvents(), fakeUpdateManager())
+        val vm = AppViewModel(AppStateManager(), env(devMode = true), FakeHookRegistry, FakeCopilotBridge, FakeOpenCodeBridge, RegistrationEvents(), fakeUpdateManager())
         runCurrent()
         assertTrue(vm.tabState.value.devMode)
     }

composeApp/src/jvmTest/kotlin/com/mikepenz/agentbelay/ui/settings/SettingsViewModelTest.kt
@@ -5,6 +5,7 @@ import com.mikepenz.agentbelay.capability.modules.ResponseCompressionCapability
 import com.mikepenz.agentbelay.capability.modules.SocraticThinkingCapability
 import com.mikepenz.agentbelay.hook.CopilotBridge
 import com.mikepenz.agentbelay.hook.HookRegistry
+import com.mikepenz.agentbelay.hook.OpenCodeBridge
 import com.mikepenz.agentbelay.hook.RegistrationEvents
 import com.mikepenz.agentbelay.model.CapabilityModuleSettings
 import com.mikepenz.agentbelay.model.CapabilitySettings
@@ -108,6 +109,18 @@ class SettingsViewModelTest {
         override fun unregisterSessionStartHook(port: Int) { sessionStartHookPorts.remove(port) }
     }
 
+    private class FakeOpenCodeBridge : OpenCodeBridge {
+        val registeredPorts: MutableSet<Int> = mutableSetOf()
+        override fun isRegistered(port: Int): Boolean = port in registeredPorts
+        override fun register(port: Int) { registeredPorts.add(port) }
+        override fun unregister(port: Int) { registeredPorts.remove(port) }
+
+        val capabilityHookPorts: MutableSet<Int> = mutableSetOf()
+        override fun isCapabilityHookRegistered(port: Int): Boolean = port in capabilityHookPorts
+        override fun registerCapabilityHook(port: Int) { capabilityHookPorts.add(port) }
+        override fun unregisterCapabilityHook(port: Int) { capabilityHookPorts.remove(port) }
+    }
+
     private fun newVm(
         bridge: FakeCopilotBridge = FakeCopilotBridge(),
         registry: FakeHookRegistry = FakeHookRegistry(),
@@ -131,6 +144,7 @@ class SettingsViewModelTest {
         val vm = SettingsViewModel(
             stateManager = state,
             copilotBridge = bridge,
+            openCodeBridge = FakeOpenCodeBridge(),
             copilotStateHolder = copilotState,
             ollamaStateHolder = ollamaState,
             riskAnalyzerLifecycle = lifecycle,